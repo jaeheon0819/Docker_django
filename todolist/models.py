fb5bed5e5ece586a446bfd3dd23835c4c7906fb3
jaeheon0819 <jhjhjh0819@naver.com>
2024-04-23 22:08:28 +0845

    final?

--- todolist/models.py
+++ todolist/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 class Todo(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
-    due_date = models.DateTimeField(blank=True, null=True)
-    completed = models. BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
+    explanation = models.TextField(blank=True, null=True)
+    end = models. BooleanField(default=False)
+    due_date = models.DateTimeField(blank=True, null=True)
+    
 
     def __str__(self):
         return self.title
