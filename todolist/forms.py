8dc776bdc9d6b975e3fd5685db055642d7a327c6
jaeheon <jhjhjh0819@naver.com>
2024-05-17 18:11:51 +0900

    test

--- todolist/forms.py
+++ todolist/forms.py
@@ -4,4 +4,4 @@ from .models import Todo
 class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
-        fields = ['title', 'explanation', 'due_date']  # 'description'을 'explanation'으로 수정
+        fields = ['title', 'explanation', 'due_date']  
